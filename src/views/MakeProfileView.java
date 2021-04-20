e06221f8628b0b4359518357339f879dffff812b
Justin Passchier <juspasschier@gmail.com>
2021-04-20 17:26:56 +0200

    Added panels and new labels into MakeProfileView

--- src/views/MakeProfileView.java
+++ src/views/MakeProfileView.java
@@ -9,18 +9,57 @@ import java.awt.event.ActionEvent;
 
 public class MakeProfileView extends SubPanel implements ActionListener {
     private JButton jbBack, jbSave;
-    private JTextField jtTest;
+    private JTextField jtFirstName, jtLastName, jtPassword;
+    private JLabel jlFirstNameLabel, jlLastNameLabel, jlPasswordLabel, welcomeLabel;
 
     public MakeProfileView(MainPanel parent, String panel_name) {
         super(parent, panel_name);
         setVisible(false);
 
-        jtTest = new JTextField(13);
+        // TOP
+        JPanel top = new JPanel();
+        top.setPreferredSize(new Dimension(1000,100));
+        top.setBorder(BorderFactory.createEmptyBorder(40, 0, 10, 0));
+
+        welcomeLabel = new JLabel("Maak hier een profiel aan:");
+        welcomeLabel.setFont(welcomeLabel.getFont().deriveFont(24.0f));
+        top.add(welcomeLabel);
+
+        // CENTER
+        jtFirstName = new JTextField(20);
+        jtLastName = new JTextField(20);
+        jtPassword = new JTextField(20);
         jbSave = new CButton(this, "opslaan", Color.black, Color.white);
         jbBack = new CButton(this, "terug naar hoofdmenu", Color.black, Color.white);
-        
-        add(new JLabel("Vul hier je naam in:"));
-        add(jtTest);
+
+        jlFirstNameLabel = new JLabel("Vul hier je voornaam in:");
+        jlLastNameLabel = new JLabel("Vul hier je achternaam in:");
+        jlPasswordLabel = new JLabel("Vul hier je wachtwoord in:");
+
+        jlFirstNameLabel.setFont(jlFirstNameLabel.getFont().deriveFont(15.0f));
+        jlLastNameLabel.setFont(jlLastNameLabel.getFont().deriveFont(15.0f));
+        jlPasswordLabel.setFont(jlPasswordLabel.getFont().deriveFont(15.0f));
+
+        // Panels to divide labels and textfields.
+        JPanel FirstName = new JPanel();
+        FirstName.setPreferredSize(new Dimension(1000,40));
+        FirstName.add(jlFirstNameLabel);
+        FirstName.add(jtFirstName);
+
+        JPanel LastName = new JPanel();
+        LastName.setPreferredSize(new Dimension(1000,40));
+        LastName.add(jlLastNameLabel);
+        LastName.add(jtLastName);
+
+        JPanel Password = new JPanel();
+        Password.setPreferredSize(new Dimension(1000,40));
+        Password.add(jlPasswordLabel);
+        Password.add(jtPassword);
+
+        add(top);
+        add(FirstName);
+        add(LastName);
+        add(Password);
         add(jbSave);
         add(jbBack);
     }
@@ -30,7 +69,7 @@ public class MakeProfileView extends SubPanel implements ActionListener {
         if (e.getSource() == jbBack) {
             changeFocus("ProfileView");
         } else if (e.getSource() == jbSave) {
-            JOptionPane.showMessageDialog(this, "Wow '" + jtTest.getText() + "' is echt een coole naam.");
+            JOptionPane.showMessageDialog(this, "Wow '" + jtFirstName.getText() + "' is echt een coole naam.");
         }
         Audio.play("../resources/click.wav");
     }
