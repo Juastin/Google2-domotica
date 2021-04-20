9f03512728e3e58781f42790b754d844942c1cc4
Justin Passchier <juspasschier@gmail.com>
2021-04-20 10:53:52 +0200

    Renamed button names

--- src/views/ImageView.java
+++ src/views/ImageView.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class ImageView extends SubPanel implements ActionListener {
-    private JButton jbStart, jbTest;
+    private JButton jbBack, jbSave;
     private JTextField jtTest;
 
     public ImageView(MainPanel parent, String panel_name) {
@@ -16,18 +16,18 @@ public class ImageView extends SubPanel implements ActionListener {
         setVisible(false);
 
         jtTest = new JTextField(13);
-        jbTest = new CButton(this, "opslaan", Color.black, Color.white);
-        jbStart = new CButton(this, "terug naar hoofdmenu", Color.black, Color.white);
+        jbSave = new CButton(this, "opslaan", Color.black, Color.white);
+        jbBack = new CButton(this, "terug naar hoofdmenu", Color.black, Color.white);
         
         add(new JLabel("Vul hier je naam in:"));
         add(jtTest);
-        add(jbTest);
-        add(jbStart);
+        add(jbSave);
+        add(jbBack);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == jbStart) {
+        if (e.getSource() == jbBack) {
             changeFocus("ProfileView");
         } else if (e.getSource() == jbTest) {
             JOptionPane.showMessageDialog(this, "Wow '" + jtTest.getText() + "' is echt een coole naam.");
