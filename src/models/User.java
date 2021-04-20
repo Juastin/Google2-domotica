1b05567fb374027fad92cea60d1fb5ef3a4e64b2
storm <storm.tersteeg@unitracer.nl>
2021-04-20 10:33:05 +0200

    Added static User to keep track of user related variables

---
+++ src/models/User.java
@@ -0,0 +1,24 @@
+package src.models;
+
+public class User {
+    private static boolean isLoggedIn = false;
+    private static String username;
+
+    public User() {}
+
+    public static void setUsername(String username) {
+        User.username = username;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setLoggedIn(boolean isLoggedIn) {
+        User.isLoggedIn = isLoggedIn;
+    }
+
+    public static boolean getLoggedIn() {
+        return isLoggedIn;
+    }
+}
