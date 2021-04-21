fe863ed6d42b214192463e3ca92debbf7532180a
storm <storm.tersteeg@unitracer.nl>
2021-04-21 11:26:21 +0200

    Added bool to enable/disable logging, added log file to gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ package src;
 import src.core.*;
 
 public class Main {
+    public static boolean enableLogging = false;
+
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
     }
--- src/core/Logging.java
+++ src/core/Logging.java
@@ -6,27 +6,40 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import src.Main;
+
+/* Logging
+
+EXAMPLE USAGE:
+Logging.logThis("test");
+
+Will log "test" locally and online using the Database class.
+[2021-04-21 11:18:10] test
+
+*/
+
 public class Logging {
     
-    public static boolean logThis(String description) {
-        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    public static void logThis(String description) {
+        if (Main.enableLogging) {
+            String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 
-        // LOG LOCAL
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("log.txt", true));
-            writer.append("[" + time + "] " + description + "\n");
-            writer.close();
-        } catch (IOException ex) {
-            System.out.println(ex);
+            // LOG LOCAL
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter("log.txt", true));
+                writer.append("[" + time + "] " + description + "\n");
+                writer.close();
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+    
+            // LOG ONLINE - IF CONNECTED
+            try {
+                Database.query("INSERT INTO Logging (time, description) VALUES ('" + time + "', '" + description + "')");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
-        // LOG ONLINE - IF CONNECTED
-        try {
-            Database.query("INSERT INTO Logging (time, description) VALUES ('" + time + "', '" + description + "')");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
     }
 
 }
