e13f77d2d69c1b637521eca140f8f7914bd938a2
Justin Passchier <juspasschier@gmail.com>
2021-04-21 14:47:41 +0200

    removed wrong comment

--- src/models/Authentication.java
+++ src/models/Authentication.java
@@ -16,7 +16,6 @@ public class Authentication {
     // @param char[] password from loginForm.
     // @param String bcryptHashString from database
     // @return BCrypt.Result
-    // Returns boolean true when char[] password is same as bcryptHashString
     // Result{details=HashData{cost=12, version=$2a$, rawSalt=d63f56f9a7ddcd562a9b4d05f775a482, rawHash=dbf6f4d1311531d1f4822d2d6f1b53bebc4258544901ab}, validFormat=true, verified=true, formatErrorMessage='null'}
     // To check result: result.verified (that returns true or false)
     public static BCrypt.Result decryptPassword(char[] password, String bcryptHashString){
