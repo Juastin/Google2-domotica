b3c92c3c65fdc174fc2d522f9d5800fe10253315
Justin Passchier <juspasschier@gmail.com>
2021-04-23 13:31:19 +0200

    Installed maven, intergration not fully complete

--- src/main/java/src/components/CButton.java
+++ src/main/java/src/components/CButton.java
@@ -4,6 +4,19 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 
+/* CButton
+
+An extention of JButton, created to:
++ Immediately give style
++ Immediately give an ActionListener
+
+Decreases the amount of code by atleast 5 lines.
+
+Example usage (in which 'this' is the ActionListener):
+CButton button = new CButton(this, "click me", Color.black, Color.white);
+
+*/
+
 public class CButton extends JButton {
 
     public CButton(ActionListener parent, String text, Color fg, Color bg) {
---
+++ src/main/java/src/views/MakeProfileView.java
@@ -0,0 +1,117 @@
+package src.views;
+import org.mindrot.jbcrypt.BCrypt;
+import src.core.*;
+import src.components.*;
+import src.system.Authentication;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MakeProfileView extends SubPanel implements ActionListener {
+    private JButton jbBack, jbSave;
+    private JTextField jtUsername;
+    private JPasswordField jtPassword;
+    private JLabel jlUsernameLabel, jlPasswordLabel, welcomeLabel;
+
+    public MakeProfileView(MainPanel parent, String panel_name) {
+        super(parent, panel_name);
+        setVisible(false);
+
+        // TOP
+        JPanel top = new JPanel();
+        top.setPreferredSize(new Dimension(1000,100));
+        top.setBorder(BorderFactory.createEmptyBorder(40, 0, 10, 0));
+
+        welcomeLabel = new JLabel("Maak hier een profiel aan:");
+        welcomeLabel.setFont(welcomeLabel.getFont().deriveFont(24.0f));
+        top.add(welcomeLabel,BorderLayout.NORTH);
+
+        // CENTER
+        jtUsername = new JTextField(20);
+        jtPassword = new JPasswordField(20);
+
+        JPanel backLabelPanel = new JPanel();
+        jbBack = new JButton("< Terug");
+        jbBack.addActionListener(this);
+        jbBack.setOpaque(false);
+        jbBack.setContentAreaFilled(false);
+        jbBack.setBorderPainted(false);
+        jbBack.setFont(jbBack.getFont().deriveFont(24.0f));
+        backLabelPanel.add(jbBack, BorderLayout.SOUTH);
+
+        JPanel savePanel = new JPanel();
+        jbSave = new CButton(this, "Profiel aanmaken", Color.black, Color.white);
+        jbSave.setFont(jbBack.getFont().deriveFont(20.0f));
+
+
+        jlUsernameLabel = new JLabel("Gebruikersnaam");
+        jlPasswordLabel = new JLabel("Wachtwoord");
+
+        jlUsernameLabel.setFont(jlUsernameLabel.getFont().deriveFont(15.0f));
+        jlPasswordLabel.setFont(jlPasswordLabel.getFont().deriveFont(15.0f));
+
+        // Panels to divide labels and textfields.
+        JPanel usernameLabel = new JPanel();
+        usernameLabel.setPreferredSize(new Dimension(1000,40));
+        usernameLabel.add(jlUsernameLabel);
+
+        JPanel username = new JPanel();
+        username.setPreferredSize(new Dimension(1000,40));
+        username.add(jtUsername);
+
+        JPanel passwordLabel = new JPanel();
+        passwordLabel.setPreferredSize(new Dimension(1000,40));
+        passwordLabel.add(jlPasswordLabel);
+
+        JPanel password = new JPanel();
+        password.setPreferredSize(new Dimension(1000,50));
+        password.add(jtPassword);
+
+        JPanel save = new JPanel();
+        save.setPreferredSize(new Dimension(1000,100));
+        save.add(jbSave);
+
+        // BOTTOM
+        JPanel bottom = new JPanel();
+        bottom.setPreferredSize(new Dimension(1000,100));
+        bottom.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+        bottom.add(backLabelPanel, BorderLayout.SOUTH);
+
+
+        add(top,BorderLayout.NORTH);
+        add(usernameLabel,BorderLayout.CENTER);
+        add(username,BorderLayout.CENTER);
+        add(passwordLabel,BorderLayout.CENTER);
+        add(password,BorderLayout.CENTER);
+        add(save,BorderLayout.CENTER);
+        add(bottom,BorderLayout.SOUTH);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == jbBack) {
+            changeFocus("ProfileView");
+        } else if (e.getSource() == jbSave) {
+            String usernameText = jtUsername.getText();
+            char[] passwordText = jtPassword.getPassword();
+            System.out.println(usernameText+" "+passwordText);
+
+            String encryptedPassword = Authentication.encryptPassword(passwordText);
+            System.out.println("encrypted: "+passwordText +" "+ encryptedPassword);
+
+            String passwordTest = "1234";
+            System.out.println("Check true if password is: "+passwordTest);
+
+            boolean result = Authentication.decryptPassword(passwordTest.toCharArray(),encryptedPassword);
+            System.out.println(result);
+        }
+        Audio.play("../resources/click.wav");
+    }
+
+    @Override
+    public void onFocus() {
+
+    }
+}
