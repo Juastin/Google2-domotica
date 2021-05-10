19a38aa640311eb9bb61e72ac8612e49e0a2931e
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2021-05-10 21:32:13 +0200

    Changed values of standardsettings
    Waarden veranderd omdat het anders niet meer klopt met de database standaardwaarden

--- src/main/java/src/views/PersonalSettingsView.java
+++ src/main/java/src/views/PersonalSettingsView.java
@@ -145,7 +145,7 @@ public class PersonalSettingsView extends View implements ActionListener, Change
         } else if (e.getSource() == jbStandardSettings) {
             int choice = JOptionPane.showConfirmDialog(this, confirmationText, "Bevestiging naar standaardinstellingen", JOptionPane.YES_NO_OPTION);
             if(choice == JOptionPane.YES_OPTION){
-                boolean result = Queries.updatePersonalSettings(25 ,16, User.getUsername());
+                boolean result = Queries.updatePersonalSettings(40,17, User.getUsername());
                 if (result) {
                     User.setStandardPersonalSettings();
                     JOptionPane.showMessageDialog(this, "Instellingen teruggezet naar standaardinstellingen");
